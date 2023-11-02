5a7317b7106c1d7850cdd24f9be544ded4a24f71
Khazixi <khazixfangirl@gmail.com>
2023-11-02 16:31:55 -0400

    Event Listeners Properly attach, Remove Button works properly

--- src/main.ts
+++ src/main.ts
@@ -29,8 +29,24 @@ const expense = (name: string, amount: number, desc: string) => `
     <h2> ${name} </h2>
     <h3> ${amount} </h3>
     <p> Description: <span> ${desc} </span> </p>
-    <button> Remove Expense </button>
+    <button data-edit="delete"> Remove Expense </button>
+    <button data-edit="change"> Edit </button>
   </div>
+`
+
+const editable_expense = (name: string, amount: number, desc: string) => `
+  <form>
+    <label for="name"> ${name} </label>
+    <input type="text" name="name"/>
+    <label for="amount"> ${amount} </label>
+    <input type="number" name="amount"/>
+    <label for="description"> Description: </label>
+    <textarea name="description">
+      ${desc}
+    </textarea>
+    <button data-edit="delete"> Remove Expense </button>
+    <button data-edit="save"> Save </button>
+  </form>
 `
 //
 // function remove_button() {
@@ -47,10 +63,20 @@ const expense = (name: string, amount: number, desc: string) => `
 // }
 
 
+function remove_expense(e: Event) {
+  const elem = e.target as HTMLButtonElement
+  const exp = elem.closest('div')
+  exp!.remove()
+}
 
 function expense_adder() {
   const expense_list = document.getElementById('expenses')
   expense_list!.insertAdjacentHTML('beforeend', expense('expense', 0, 'New Expense'))
+  const exp = expense_list!.lastElementChild!
+  const removalButton = exp.querySelector('[data-edit="delete"]')
+  removalButton!.addEventListener('click', (e) => remove_expense(e))
+  const editButton = exp.querySelector('[data-edit="change"]')
+  editButton!.addEventListener('click', (e) => console.log(`edit ${e.target}`))
 }
 
 document.getElementById('expense-adder')!.addEventListener('click', expense_adder)
